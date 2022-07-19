d34a3f1c3121eed0f36f49ae19fd2a76edf1a73b
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2022-07-19 23:47:05 +0100

    another test

--- APP/content/urls.py
+++ APP/content/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 # App name
-app_name = 'Content'
+app_name = 'content'
 urlpatterns = [
         path('', views.IndexView.as_view(), name='index'),
         path('hub', views.HubView.as_view(), name='hub'),
