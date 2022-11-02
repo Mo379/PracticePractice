0e4783ec09edb303e4f09869cfc1cca7a217a044
Ubuntu <ubuntu@ip-172-31-39-78.eu-west-1.compute.internal>
2022-11-02 09:47:19 +0000

    MM_config more

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 docker compose up -d 
 mkdir -p extras/data/MM/volumes/app/mattermost/{config,data,logs,plugins,client/plugins,bleve-indexes}
 sudo chown -R 2000:2000 extras/data/MM/volumes/app/mattermost
+sudo chown -R 2000:2000 extras/includes/MM_config
 cd APP_MatterMost
 docker compose -f docker-compose.yml -f docker-compose.without-nginx.yml up -d
 cd ../
