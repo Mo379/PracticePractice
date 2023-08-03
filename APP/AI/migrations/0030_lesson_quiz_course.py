8634b52aeee2a361cd66f8cb78dcb15f54f19c34
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2023-08-03 22:49:20 +0100

    minor

--- APP/AI/migrations/0030_lesson_quiz_course.py
+++ APP/AI/migrations/0030_lesson_quiz_course.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='lesson_quiz',
             name='course',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, related_name='AI_quiz_course', to='content.course'),
+            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, related_name='AI_quiz_course', to='content.course', null=True),
         ),
     ]
