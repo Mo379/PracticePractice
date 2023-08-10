833096f2a6dcfc65661f347fce66c9de80b958af
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2023-08-10 08:42:16 +0100

    Fixed sidebar

--- APP/dashboard/static/dashboard/js/sb-admin-2.js
+++ APP/dashboard/static/dashboard/js/sb-admin-2.js
@@ -7,23 +7,16 @@
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
-    };
-    
-    // Toggle the side navigation when window is resized below 480px
-    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-      $("body").addClass("sidebar-toggled");
-      $(".sidebar").addClass("toggled");
-      $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
-
+    
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
     if ($(window).width() > 768) {
@@ -54,3 +47,4 @@
   });
 
 })(jQuery); // End of use strict
+
