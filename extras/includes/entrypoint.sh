8025d4aa73ebd1c7c5d478b7a99421cf0d439a69
Mustafa Omar <M@LAPTOP-JSFION9Q.localdomain>
2023-09-09 13:11:41 +0100

    debug entrypoint

--- extras/includes/entrypoint.sh
+++ extras/includes/entrypoint.sh
@@ -2,4 +2,4 @@
 cd /var/www/html/
 apache2ctl -D FOREGROUND
 useradd -m -s /bin/bash celeryuser
-python3 -m celery -A PP2 worker --uid=celeryuser -l info -D
+python3 -m celery -A PP2 worker --uid=celeryuser -l info
